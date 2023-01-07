4daab6fed3feb1fe23df195cd58dd5d06313f4ed
b0mtrady <b0mtrady@bornLazy.com>
2023-01-07 21:38:25 +0000

    added a bunch of js that will go in the garbage when replaced by Rust/wasm

--- ode/white-noise-generator.js
+++ ode/white-noise-generator.js
@@ -4,10 +4,17 @@ class WhiteNoiseProcessor extends AudioWorkletProcessor {
     }
 }
 
-    process(inputList, outputList, parameters)
+    // input and output are float32 arrays each of which 
+    // contains audio data for one channel 
+    process(inputs, outputs, parameters) {
+        const input = inputs[0];
+        const output = outputs[0];
+        const channelCount = input.length;
+    }
 
 
 
 
 registerProcessor("white-noise-generator", WhiteNoiseProcessor);
+
 
